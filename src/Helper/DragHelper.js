02e497db33b5912d706bbf3e9dd6a8c43f474321
Jerome Beckett <jeromebeckett@yahoo.co.uk>
2023-11-09 16:33:31 +0000

    Only following links when carousel is not being dragged

--- src/Helper/DragHelper.js
+++ src/Helper/DragHelper.js
@@ -7,12 +7,24 @@ export default class DragHelper {
 		this.x = 0;
 		this.mouseX = 0;
 		this.startMouseX = 0;
+		this.movedX = 0;
 		this.moving = false;
+		this.clickedLink = false;
 	}
 
 	addEvents(getCurrentPos, checkBoundaries) {
+		this.wrapper.children().click((event) => {
+			event.preventDefault();
+
+			if(!this.moving && Math.abs(this.movedX) < 2) {
+				let link = jQuery(event.target).closest('a').attr('href');
+				if(link != undefined) location.href = link;
+			}
+		});
+
 		this.wrapper.on("mousedown", (event) => {
 			this.moving = true;
+			this.movedX = 0;
 			this.startX = getCurrentPos();
 			this.startMouseX = event.clientX;
 		});
@@ -24,8 +36,8 @@ export default class DragHelper {
 		document.addEventListener("mousemove", (event) => {
 			event.preventDefault();
 			if(this.moving) {
-
-				this.x = this.startX + this.startMouseX - event.clientX;
+				this.movedX = this.startMouseX - event.clientX;
+				this.x = this.startX + this.movedX;
 
 				const max = this.wrapper.outerWidth() - this.inner.outerWidth();
 
