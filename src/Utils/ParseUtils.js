3c6403d98bdc7739308c8bf60783c9705c24ef1d
Jerome Beckett <jeromebeckett@yahoo.co.uk>
2024-01-10 15:42:11 +0000

    Rounding border/padding subpixels to 1DP instead of integer

--- src/Utils/ParseUtils.js
+++ src/Utils/ParseUtils.js
@@ -1,7 +1,7 @@
 
 export default class ParseUtils {
 	static pixelsToInt(pixels) {
-		// Round down subpixels to avoid incorrect comparisons
-		return Math.floor(pixels.replace('px', ''));
+		// Round down subpixels past 1DP to avoid incorrect comparisons
+		return Math.floor(pixels.replace('px', '') * 10) / 10;
 	}
 }
